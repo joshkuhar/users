254df9e762b8b9b9e5ec7247f9c47c379fd85c54
Josh Kuhar <jdkuhar@gmail.com>
2016-09-19 15:34:22 -0400

    update

--- index.js
+++ index.js
@@ -34,6 +34,8 @@ var strategy = new BasicStrategy(function(username, password, callback) {
 passport.use(strategy);
 
 
+
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
@@ -44,6 +46,8 @@ var app = express();
 var jsonParser = bodyParser.json();
 
 var bcrypt = require('bcryptjs');
+
+
 
 app.post('/users', jsonParser, function(req, res) {
     if (!req.body) {
@@ -128,17 +132,16 @@ app.post('/users', jsonParser, function(req, res) {
     });
 });
 
-var bcrypt = require('bcryptjs');
 
-UserSchema.methods.validatePassword = function(password, callback) {
-    bcrypt.compare(password, this.password, function(err, isValid) {
-        if (err) {
-            callback(err);
-            return;
-        }
-        callback(null, isValid);
+
+app.use(passport.initialize());
+
+
+app.get('/hidden', passport.authenticate('basic', {session: false}), function(req, res) {
+    res.json({
+        message: 'Luke... I am your father'
     });
-};
+});
 
 mongoose.connect('mongodb://localhost/auth').then(function() {
     app.listen(process.env.PORT || 8080);
